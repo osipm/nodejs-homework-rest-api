a5573eeb6f6751fefa8a5dae5d67472f31f60d8f
osipm <54916138+osipm@users.noreply.github.com>
2022-01-27 23:37:13 +0200

    hw3

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const app = require("./app");
-const { PORT = 3000 } = process.env;
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`);
-});
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const { DB__HOST } = process.env;
+
+mongoose
+  .connect(DB__HOST)
+  .then(() => console.log("database connect success"))
+  .catch((error) => console.log(error.message));
+
+// const app = require("./app");
+// const { PORT = 3000 } = process.env;
+// app.listen(PORT, () => {
+//   console.log(`Server running. Use our API on port: ${PORT}`);
+// });
