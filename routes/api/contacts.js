758baa8f76760076532e2b380ad29e654bbd3c0f
osipm <54916138+osipm@users.noreply.github.com>
2022-01-28 23:41:24 +0200

    find

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,7 +2,7 @@ const express = require("express");
 const CreateError = require("http-errors");
 const joi = require("joi");
 
-const contacts = require("../../models/contacts");
+const Contact = require("../../models/contact");
 
 const contactsShema = joi.object({
   name: joi.string().required(),
@@ -14,68 +14,68 @@ const router = express.Router();
 
 router.get("/", async (req, res, next) => {
   try {
-    const result = await contacts.listContacts();
+    const result = await Contact.find();
     res.json(result);
   } catch (error) {
     next(error);
   }
 });
 
-router.get("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contacts.getContactById(contactId);
-    if (!result) {
-      throw new CreateError(404, "Not found");
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+// router.get("/:contactId", async (req, res, next) => {
+//   try {
+//     const { contactId } = req.params;
+//     const result = await contacts.getContactById(contactId);
+//     if (!result) {
+//       throw new CreateError(404, "Not found");
+//     }
+//     res.json(result);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
-router.post("/", async (req, res, next) => {
-  try {
-    const { error } = contactsShema.validate(req.body);
-    if (error) {
-      throw new CreateError(400, error.massege);
-    }
-    const result = await contacts.addContact(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+// router.post("/", async (req, res, next) => {
+//   try {
+//     const { error } = contactsShema.validate(req.body);
+//     if (error) {
+//       throw new CreateError(400, error.massege);
+//     }
+//     const result = await contacts.addContact(req.body);
+//     res.status(201).json(result);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
-router.delete("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contacts.removeContact(contactId);
-    if (!result) {
-      throw new CreateError(404, "Not found");
-    }
-    res.json({ message: "contact deleted" });
-  } catch (error) {
-    next(error);
-  }
-});
+// router.delete("/:contactId", async (req, res, next) => {
+//   try {
+//     const { contactId } = req.params;
+//     const result = await contacts.removeContact(contactId);
+//     if (!result) {
+//       throw new CreateError(404, "Not found");
+//     }
+//     res.json({ message: "contact deleted" });
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
-router.put("/:contactId", async (req, res, next) => {
-  try {
-    const { error } = contactsShema.validate(req.body);
-    if (error) {
-      throw new CreateError(400, error.massege);
-    }
-    const { contactId } = req.params;
+// router.put("/:contactId", async (req, res, next) => {
+//   try {
+//     const { error } = contactsShema.validate(req.body);
+//     if (error) {
+//       throw new CreateError(400, error.massege);
+//     }
+//     const { contactId } = req.params;
 
-    const result = await contacts.updateContact(contactId, req.body);
-    if (!result) {
-      throw new CreateError(404, "Not found");
-    }
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+//     const result = await contacts.updateContact(contactId, req.body);
+//     if (!result) {
+//       throw new CreateError(404, "Not found");
+//     }
+//     res.status(201).json(result);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const mongoose = require("mongoose");
+const app = require("./app");
 
-require("dotenv").config();
-
-const { DB__HOST } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
-  .connect(DB__HOST)
-  .then(() => console.log("database connect success"))
-  .catch((error) => console.log(error.message));
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("database connect success");
+    return app.listen(PORT);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
 
 // const app = require("./app");
 // const { PORT = 3000 } = process.env;
